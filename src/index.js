9c1f327e8b2deb71f1fc460c75116e64b5746eeb
leo-jesus <llxdgb1@gmail.com>
2021-03-18 14:29:20 -0300

    challenge concluded

--- src/index.js
+++ src/index.js
@@ -11,18 +11,66 @@ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+  if (!user) {
+    return response.status(404).json({ error: "User not found!" })
+
+  }
+  request.user = user;
+  return next();
 }
+
 
 function checksCreateTodosUserAvailability(request, response, next) {
   // Complete aqui
+  const { user } = request;
+  if (!user.pro && user.todos < 10 || user.pro) {
+    return next();
+  }
+
+  return response.status(403).json();
 }
 
 function checksTodoExists(request, response, next) {
   // Complete aqui
+  
+  const { username } = request.headers;
+  const { id } = request.params;
+  if (!validate(id, 4)) { 
+    /* HERE: I lost some time tring to to validate uuid until figure out the validate parameter was require uuid (hauhauhauah)
+    So, I searched quite a lot and found a link (https://www.npmjs.com/package/uuid-validate) which explain what validate does.*/ 
+
+    return response.status(400).json({ error: "Invalid id" })
+  }
+
+  const user = users.find(user => user.username === username);
+  if (!user) {
+    return response.status(404).json({ error: "User not found" });
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+  if (!todo) {
+    return response.status(404).json({ error: "Todo not found" });
+  }
+
+  request.user = user;
+  request.todo = todo;
+  return next();
+
 }
 
 function findUserById(request, response, next) {
   // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+  if (!user) {
+    return response.status(404).json({ error: "User not found!" })
+  }
+  request.user = user;
+  return next();
+
 }
 
 app.post('/users', (request, response) => {
